27b9d6c9ad6d391774712d854efd42c70ae0a575
woojoo <dnwn@justbuild.kr>
2026-03-29 19:50:06 +0900

    feat: 유저 생성 시 입력값 검증 추가

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -16,6 +16,9 @@ const getUserById = (req, res) => {
 
 const createUser = (req, res) => {
   const { name, email, password } = req.body
+  if (!name || !email || !password) {
+    return res.status(400).json({ message: 'name, email, password are required' })
+  }
   const newUser = { id: users.length + 1, name, email, password }
   users.push(newUser)
   res.status(201).json(newUser)
